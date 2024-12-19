9531daf5bfc50e9a45f5983f2cdcf72baab7fa94
dagmfre <dagmfre@gmail.com>
2024-12-19 16:51:13 +0300

    add initial files and configurations for project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Vite React App</h1>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
